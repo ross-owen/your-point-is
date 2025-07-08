61d86ea5b5283aa9670edaa43bd34765d4172f39
Ross L. Owen <rowen@byupathway.edu>
2025-07-07 21:21:01 -0600

    stub express and ejs and google auth using passport as well as mongoose for database access

---
+++ controllers/authController.js
@@ -0,0 +1,26 @@
+﻿const utilities = require('../utilities')
+require('dotenv').config()
+
+async function buildLogin(req, res) {
+    let nav = await utilities.getNav()
+    res.render('auth/login', {
+        title: 'Login',
+        nav,
+        errors: null,
+    })
+}
+
+async function accountLogin(req, res) {
+    // todo
+}
+
+async function accountLogout(req, res) {
+    // todo
+    res.redirect('/auth/login')
+}
+
+module.exports = {
+    buildLogin,
+    accountLogin,
+    accountLogout,
+}
