47f296fe60d4b41a0330c0733cf2d073980f6aae
nicoopariente <hnpf2019@gmail.com>
2025-07-18 09:32:39 -0300

    creating_new_room

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,9 +1,27 @@
-﻿require('dotenv').config()
+﻿require("dotenv").config();
+const Room = require("./models/Room");
 
 async function buildDashboard(req, res) {
-    res.render("dashboard/index", {title: "Dashboard"})
+  res.render("dashboard/index", { title: "Dashboard" });
+}
+
+async function createRoom(req, res) {
+  try {
+    const room_code = "";
+    const newRoom = new Room({
+      room_code: room_code,
+      host_name: req.user.displayName,
+      host_socket_id: "",
+      guests: [],
+    });
+    await newRoom.save();
+    res.redirect(`/room?${room_code}`);
+  } catch (error) {
+    res.status(500).send("Failed to create room");
+  }
 }
 
 module.exports = {
-    buildDashboard,
-}
+  buildDashboard,
+  createRoom,
+};
